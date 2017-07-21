98980b3a5431a1b779d40f708c635952ac5b5849
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2017-07-21 02:18:23 +0200

    Include projection world in asds. Add setup function.

--- plan_transformation/lisp/setup.lisp
+++ plan_transformation/lisp/setup.lisp
@@ -11,8 +11,8 @@
 ;;;       notice, this list of conditions and the following disclaimer in the
 ;;;       documentation and/or other materials provided with the distribution.
 ;;;     * Neither the name of the Intelligent Autonomous Systems Group/
-;;;       Technische Universitaet Muenchen nor the names of its contributors 
-;;;       may be used to endorse or promote products derived from this software 
+;;;       Technische Universitaet Muenchen nor the names of its contributors
+;;;       may be used to endorse or promote products derived from this software
 ;;;       without specific prior written permission.
 ;;;
 ;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -27,11 +27,34 @@
 ;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ;;; POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :cl-user)
+(in-package :plt)
 
-(defpackage :plan-transformation-tests
-  (:nicknames :plt-tests)
-  (:use :cpl :cpl-impl :alexandria :roslisp)
-  (:export
-   #:generate-data
-   #:setup))
+(defun init-projection ()
+  (def-fact-group costmap-metadata ()
+    (<- (location-costmap:costmap-size 12 12))
+    (<- (location-costmap:costmap-origin -6 -6))
+    (<- (location-costmap:costmap-resolution 0.05))
+
+    (<- (location-costmap:costmap-padding 0.2))
+    (<- (location-costmap:costmap-manipulation-padding 0.2))
+    (<- (location-costmap:costmap-in-reach-distance 0.6))
+    (<- (location-costmap:costmap-reach-minimal-distance 0.2)))
+
+  (setf cram-bullet-reasoning-belief-state:*robot-parameter* "robot_description")
+  (setf cram-bullet-reasoning-belief-state:*kitchen-parameter* "no_urdf_for_kitchen")
+
+  (sem-map:get-semantic-map)
+
+  (cram-occasions-events:clear-belief)
+
+  (setf cram-tf:*tf-default-timeout* 2.0)
+
+  (setf prolog:*break-on-lisp-errors* t))
+
+(roslisp-utilities:register-ros-init-function init-projection)
+
+;; (defmethod location-costmap:on-visualize-costmap opengl ((map location-costmap:location-costmap))
+;;   (btr:add-costmap-function-object map))
+
+;; (defmethod location-costmap:on-visualize-costmap-sample opengl ((point cl-transforms:3d-vector))
+;;   (btr:add-costmap-sample-object point))
--- plan_transformation_tests/lisp/data-generator.lisp
+++ plan_transformation_tests/lisp/data-generator.lisp
@@ -1,17 +1,34 @@
+;;;
+;;; Copyright (c) 2017, Arthur Niedzwiecki <niedzwiecki@uni-bremen.de>
+;;; All rights reserved.
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;;
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name of the Intelligent Autonomous Systems Group/
+;;;       Technische Universitaet Muenchen nor the names of its contributors 
+;;;       may be used to endorse or promote products derived from this software 
+;;;       without specific prior written permission.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+
 (in-package :plt-tests)
 
-(defun generate-data (input-data)
-  (top-level-root input-data))
-
-(def-top-level-cram-function top-level-root (test-data-top-level)
-  (first-node test-data-top-level)
-  (second-node test-data-top-level))
-
-(def-cram-function first-node (test-data-first-node)
-  (format t test-data-first-node))
-
-(def-cram-function second-node (test-data-second-node)
-  (third-node test-data-second-node))
-
-(def-cram-function third-node (test-data-third-node)
-  (format t (concatenate 'string test-data-third-node " teststring")))
+(defun generate-data ()
+  ;; TODO
+  )
